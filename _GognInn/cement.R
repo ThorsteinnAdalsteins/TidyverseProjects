25a28e7cd58bf9d45120a20d951949ced80bc710
ThorsteinnAdalsteins <ThorsteinnAdalsteins@users.noreply.github.com>
2019-02-23 09:00:12 +0000

    Create cement.R

---
+++ _GognInn/cement.R
@@ -0,0 +1,9 @@
+
+concrete.mixes <- tribble(
+  ~concrete.type, ~cement.kg, ~sand.kg, ~stone.kg, ~water.kg,
+  #---#-
+  '3000psi', 234, 707, 725, 132,
+  '3500psi', 326, 961, 867, 150 ,
+  '4000psi', 368, 1081, 737, 160,
+  '4500psi', 1, 2.26, 1.73, 0.5
+)
